ec487553de5b42996ffe30c62593f227b9ec40fd
Dmitry <solonkou4eba@mai.ru>
2022-02-18 20:19:10 +0300

    Сделанная домашка

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DAL3;
+using System.Threading.Tasks;
+
+namespace WebApplicationHomeWork.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet("getBooks")]
+        public List<Game> GetAllBooks(int bookId)
+        {
+            return new List<Game>()
+            {
+                new Game()
+                {
+                    Id = new Random().Next(100),
+                    Name = "The whitcher 3. Whild Hung",
+                    Janr = "Приключение"
+                },
+                new Game()
+                {
+                    Id = new Random().Next(100),
+                    Name = "PUBG",
+                    Janr = "Батл рояль"
+                }
+            };
+        }
+    }
+}
